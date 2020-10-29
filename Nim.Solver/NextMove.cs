093a1fbb653df571b52801ac1c78ec504944b231
Justin L. Brown <justin.brown@cpchem.com>
2020-10-29 10:02:35 -0500

    Added comments to the solver.

--- Nim.Solver/NextMove.cs
+++ Nim.Solver/NextMove.cs
@@ -25,28 +25,35 @@ namespace Nim.Solver
                 throw new ArgumentNullException(nameof(heaps));
             }
 
+            // When at most one heap has more than one object left, use the endgame logic.
             var isEndgameNear = heaps.Count(x => x > 1) <= 1;
-
             if (isEndgameNear)
             {
                 var isOdd = (heaps.Count(x => x > 0) % 2) == 1;
                 var maxHeapSize = heaps.Max();
                 var indexOfMaxHeapSize = heaps.ToList().FindIndex(x => x == maxHeapSize);
+
+                // An odd number of heaps with at most one object is a losing position.
                 if (maxHeapSize == 1 && isOdd)
                 {
                     return null;
                 }
 
+                // The winning move is to leave an even number of heaps with one object.
                 return new Move(indexOfMaxHeapSize, maxHeapSize - (isOdd ? 1 : 0));
             }
 
+            // Calculate the "Nim sum" of the heap sizes.
             var nimSum = NimSum(heaps);
 
+            // A Nim sum of zero is a losing position.
             if (nimSum == 0)
             {
                 return null;
             }
 
+            // Check each heap size to see if its binary digital sum with the Nim sum would be smaller.
+            // Once a heap is found, the winning move is to reduce that heap to the binary digital sum.
             for (var i = 0; i < heaps.Count; i++)
             {
                 var heapSize = heaps.ElementAt(i);
