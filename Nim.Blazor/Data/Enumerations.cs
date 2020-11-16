971c3bdb351c2de725a559e880be2ce8dacecf0c
Justin L. Brown <justin.brown@cpchem.com>
2020-11-16 15:39:44 -0600

    Added a Nim game screen.

---
+++ Nim.Blazor/Data/Enumerations.cs
@@ -0,0 +1,23 @@
+﻿namespace Nim.Blazor.Data
+{
+    /// <summary>
+    /// All possible game states.
+    /// </summary>
+    public enum NimState
+    {
+        /// <summary>
+        /// The user can enter the desired starting heap sizes.
+        /// </summary>
+        Setup = 0,
+
+        /// <summary>
+        /// The game is being played.
+        /// </summary>
+        InProgress = 1,
+
+        /// <summary>
+        /// The game is finished.
+        /// </summary>
+        Done = 2
+    }
+}
